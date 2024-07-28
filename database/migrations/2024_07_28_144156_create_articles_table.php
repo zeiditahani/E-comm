37d545356e2e3810328edca949e6dd4e2d94f777
zeiditahani <129570471+zeiditahani@users.noreply.github.com>
2024-07-28 16:32:40 +0100

    first commit

---
+++ database/migrations/2024_07_28_144156_create_articles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->integer('panier_id');
+            $table->integer('produit_id');
+            $table->string('image_produit');
+            $table->string('nom_produit');
+            $table->integer('quantite');
+            $table->float('prix');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
